2b4434e463de7fbc305ccd14eecd5dd4781c33b5
escapevelocity17321 <escapevelocity17321@gmail.com>
2025-02-05 15:33:00 -0500

    Adds readme to idam replica

--- Terraform/gcp/compose/server-admin-idam_replica/outputs.tf
+++ Terraform/gcp/compose/server-admin-idam_replica/outputs.tf
@@ -3,6 +3,7 @@
 #--------------------------------------------------
 
 output "private_ip" {
+  description = "Idam Replica Private IP Address"
   value = google_compute_instance.idam_replica_server.network_interface[0].network_ip
   sensitive = true
 }
