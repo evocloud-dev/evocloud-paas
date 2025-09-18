0fe5785564d6f5a9fbd786a812ae13c12bdac8da
escapevelocity17321 <escapevelocity17321@gmail.com>
2025-09-17 22:57:56 -0400

    Modifications for Taskfile in oci and gcp folders

---
+++ Terraform/oci/compose/cluster-talos-standalone-helper/version.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">=7.0.0"
+    }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "< 0.9.0"
+    }
+  }
+}
